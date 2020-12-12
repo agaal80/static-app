539f73f8a95e8585fdc0cdc61d813e3c09d4b8ad
agaal80 <agaal80@gmail.com>
2020-12-12 22:50:54 +0100

    Angular routing added to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosComponent } from '../app/media/photos/photos.component';
+import { AlbumsComponent } from '../app/media/albums/albums.component';
+
+const appRoutes: Routes = [
+  {path: 'photos', component: PhotosComponent},
+  {path: 'albums', component: AlbumsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent,
+    AlbumsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
